ba0461e0ecc1ff5260825369c30b5ab90a817739
Aquino0 <cristopheraquino20@gmail.com>
2026-03-23 20:32:58 -0300

    ajuste simples que aprendi durante a aula

--- Linguagem_C/operadores_e_conceitos_de_expressão.c
+++ Linguagem_C/operadores_e_conceitos_de_expressão.c
@@ -5,8 +5,7 @@ int main()
     r1 = 5 + 10 * 2;
     r2 = (5 + 10) * 2;
     printf("o resultado 1 é: %.2f\n O resultado 2 é: %.2f", r1, r2);*/
-    float nota1 = 8.0;
-    float nota2 = 6.0;
+    float nota1 = 8.0, nota2 = 6.0;
     float media_errada = nota1 + nota2 / 2;
     float media_certa = (nota1 + nota2) / 2;
     printf("A média errada é: %.1f\n", media_errada);
