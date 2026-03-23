2f51174385ba8e29579c2ffc0ebaaec8014c5ea8
Aquino0 <cristopheraquino20@gmail.com>
2026-03-23 19:36:35 -0300

    Formatação dos campos

---
+++ Linguagem_C/formataçãodo_campo_de_formatação.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    /*float preço = 12.5;
+    printf("o preço é %.2f", preço);*/
+    printf("%-10s %-5s\n", "Produtos", "QTD");
+    printf("%-10s %-5d\n", "Morango", 5);
+    printf("%-10s %-5d\n", "Laranja", 10);
+    return 0;
+}
