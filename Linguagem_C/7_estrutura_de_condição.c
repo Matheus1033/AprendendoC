073810045ce3488d66953746648024d96103cb02
Aquino0 <cristopheraquino20@gmail.com>
2026-05-01 13:21:02 -0300

    estuturas de condição

--- Linguagem_C/7_estrutura_de_condição.c
+++ Linguagem_C/7_estrutura_de_condição.c
@@ -47,4 +47,37 @@ while ( i <= 10 )		 // inicialmente testa a condição i <= 10
         i++; 			 // que existirem dentro do bloco { }
 }
 printf(“O programa segue a partir daqui, quando sair do loop (while)”);
+
+do
+comando;		// com um único comando a ser repetido
+while (condição);
+
+do
+{
+        comando1;
+        comando2;	// vários comandos a serem repetidos delimitados pelas chaves
+        comando N;
+}
+while (condição);
+
+i = 1;
+do
+{
+        printf(“%d\n”,i);	// inicialmente executa essas instruções
+        i++;			// pelo menos uma vez para depois testar
+} while ( i <= 10 );
+
+for (inicialização;teste;incremento)
+comando;
+
+for (inicialização;teste;incremento)
+{
+        comando1;
+        comando2;
+        comandoN;
+}
+int i;
+for (i = 1; i <= 10; i++)
+        printf(“%d\n”,i);	// não é necessário o uso das chaves uma
+                                     // vez que só há um comando a ser repetido
 */
